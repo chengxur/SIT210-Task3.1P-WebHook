a3da379698648061c46c13f29659ff1a96f6b1c7
chengxur <chengxur@deakin.edu.au>
2021-04-22 12:53:48 +0000

    WebHook

---
+++ Take3Light_detection.ino
@@ -0,0 +1,21 @@
+
+#define PIN_A 0
+#define PIN_D 2
+ 
+void setup() 
+{
+  Serial.begin(9600);
+}
+ 
+void loop() 
+{
+  int val;
+  val=analogRead(PIN_A);
+  val= map(val,0,1023,99,0);
+  Serial.print("a:");
+  Serial.print(val);
+  Serial.print(", d:");
+  val=digitalRead(PIN_D);
+  Serial.println(val);
+  delay(500);
+}
